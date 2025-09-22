a46a313ef0d5db0daa1cd62b6c5c7d6900870217
Vinicius Campos <viniciuscampos4@gmail.com>
2025-09-22 01:53:27 -0300

    feat: create hello Function, add HelloHandler into CDK Stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,16 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_22_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+    });
   }
 }
