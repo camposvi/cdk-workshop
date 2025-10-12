aeec1723205c9d7f0d1cdcea023de302c08dfc7e
Vinicius Campos <viniciuscampos4@gmail.com>
2025-10-12 17:12:20 -0300

    feat: implement hitCounter constructor

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,4 +1,5 @@
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export interface HitCounterProps {
@@ -6,7 +7,27 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
+  public readonly handler: Function;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
+
+    const table = new Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: AttributeType.STRING },
+    });
+
+    this.handler = new Function(this, 'HitCounterHandler', {
+      runtime: Runtime.NODEJS_22_X,
+      handler: 'hitcounter.handler',
+      code: Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(this.handler);
+
+    props.downstream.grantInvoke(this.handler);
   }
 }
