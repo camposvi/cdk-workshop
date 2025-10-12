c0cc7e0b40159a0402603790718b35a9b0cd9b06
Vinicius Campos <viniciuscampos4@gmail.com>
2025-10-12 17:11:43 -0300

    feat: add HitCounter constructor

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,8 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -10,6 +12,10 @@ export class CdkWorkshopStack extends Stack {
       runtime: Runtime.NODEJS_22_X,
       code: Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello,
     });
 
     const gateway = new LambdaRestApi(this, 'Endpoint', {
